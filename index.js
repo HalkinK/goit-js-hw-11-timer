2ff2c0eb3692eed46d2f487156e4cddee240d1c9
Konstantin Halkin <constantingal@gmail.com>
2021-05-26 00:24:40 +0300

    changes

--- index.js
+++ index.js
@@ -1,38 +1,3 @@
-class CountdownTimer {
-    constructor({ selector, targetDate }) {
-        this.selector = selector;
-        this.targetDate = targetDate;
-    }
-
-    start() {
-        const startDate = this.targetDate;
-
-        setInterval(() => {
-            const currentDate = Date.now();
-            const timeDiff = startDate - currentDate;
-            const { days, hours, mins, secs } = getDateComponents(timeDiff);
-
-            const timer_1 = document.querySelector('#timer-1');
-            const day = timer_1.querySelector('[data-value="days"]');
-            day.textContent = days;
-            const hour = document.querySelector('[data-value="hours"]');
-            hour.textContent = hours;
-            const min = document.querySelector('[data-value="mins"]');
-            min.textContent = mins;
-            const sec = document.querySelector('[data-value="secs"]');
-            sec.textContent = secs;
-            
-        }, 1000);
-    };
-};
-
-const countdown = new CountdownTimer({
-  selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2021'),
-});
-
-countdown.start();
-
 function getDateComponents(time) {
     const days = Math.floor(time / (1000 * 60 * 60 * 24));    
     const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));    
@@ -41,3 +6,55 @@ function getDateComponents(time) {
 
     return { days, hours, mins, secs };
 }
+
+class CountdownTimer {
+    constructor({ selector, targetDate }) {
+        this.selector = selector;
+        this.targetDate = targetDate;
+        timerId: null;
+    }
+    
+
+    start() {
+        const startDate = this.targetDate;
+
+        const timer_1 = document.querySelector(this.selector);
+        const day = timer_1.querySelector('[data-value="days"]');
+        const hour = timer_1.querySelector('[data-value="hours"]');
+        const min = timer_1.querySelector('[data-value="mins"]');
+        const sec = timer_1.querySelector('[data-value="secs"]');
+
+        // let timerId = null;
+
+        this.timerId = setInterval(() => {
+            const currentDate = Date.now();
+            const timeDiff = startDate - currentDate;
+            const { days, hours, mins, secs } = getDateComponents(timeDiff);
+
+            // const timer_1 = document.querySelector(this.selector);
+            // const day = timer_1.querySelector('[data-value="days"]');
+            day.textContent = days;
+            // const hour = timer_1.querySelector('[data-value="hours"]');
+            hour.textContent = hours;
+            // const min = timer_1.querySelector('[data-value="mins"]');
+            min.textContent = mins;
+            // const sec = timer_1.querySelector('[data-value="secs"]');
+            sec.textContent = secs;         
+          
+           
+       }, 1000);
+        
+          if (getDateComponents(this.timeDiff) <= 0) {
+               clearInterval(this.timerId);
+        };
+       
+    };
+
+};
+
+const countdown = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('May 26, 2021, 00:24:00'),
+});
+
+countdown.start();
